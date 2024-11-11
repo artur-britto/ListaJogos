144eac66c3b4bb7a058363163aaba3433518ca37
artur-britto <britto.monteiro23@gmail.com>
2024-11-11 11:23:00 -0300

    Projeto criado

---
+++ src/main/java/com/projetojogos/listajogos/ListajogosApplication.java
@@ -0,0 +1,13 @@
+package com.projetojogos.listajogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListajogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListajogosApplication.class, args);
+	}
+
+}
